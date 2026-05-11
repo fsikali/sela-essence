a9be4f0a7cf5cc89a90e82eb912f21e30178c390
fsikali <flemmingsikali499@gmail.com>
2026-05-11 07:25:32 +0300

    remove deprecated about page to streamline routing structure

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/about/page.tsx
-export default function About() {
-  return (
-    <div>
-      <h1>Account</h1>
-      <p>Information about Sela Essence will go here.</p>
-    </div>
-  );
-}
